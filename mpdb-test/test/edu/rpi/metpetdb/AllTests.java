44d31d876ef6897ec3ceb40ac1d5781f56b98057
anthony <anthony>
2008-01-19 20:18:01 +0000

    added a started test for sample details

--- mpdb-test/test/edu/rpi/metpetdb/AllTests.java
+++ mpdb-test/test/edu/rpi/metpetdb/AllTests.java
@@ -2,14 +2,13 @@ package edu.rpi.metpetdb;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import edu.rpi.metpetdb.server.AllImplTest;
 
 public class AllTests {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for edu.rpi.metpetdb.server.test");
 		//$JUnit-BEGIN$
-		suite.addTest(AllImplTest.suite());
+		//suite.addTest(AllImplTest.suite());
 		//$JUnit-END$
 		return suite;
 	}
