d4fd402d2d71305ef8439483d3d910f422cdcecf
anthony <anthony>
2008-02-03 18:40:48 +0000

    added an ant script to automatically setup the database, run the test, and restore the database added some more client side test and verified they all work

--- mpdb-test-client/test/edu/rpi/metpetdb/AllTests.java
+++ mpdb-test-client/test/edu/rpi/metpetdb/AllTests.java
@@ -2,13 +2,14 @@ package edu.rpi.metpetdb;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import edu.rpi.metpetdb.client.ui.object.details.SampleDetailsTest;
 
 public class AllTests {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for edu.rpi.metpetdb.server.test");
 		//$JUnit-BEGIN$
-		//suite.addTest(AllImplTest.suite());
+		suite.addTestSuite(SampleDetailsTest.class);
 		//$JUnit-END$
 		return suite;
 	}
--- mpdb-test-client/test/edu/rpi/metpetdb/client/ui/object/details/SampleDetailsTest.java
+++ mpdb-test-client/test/edu/rpi/metpetdb/client/ui/object/details/SampleDetailsTest.java
@@ -1,5 +1,11 @@
 package edu.rpi.metpetdb.client.ui.object.details;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.gwtwidgets.client.ui.pagination.PaginationParameters;
+import org.gwtwidgets.client.ui.pagination.Results;
+
 import com.google.gwt.junit.client.GWTTestCase;
 
 import edu.rpi.metpetdb.client.model.Sample;
@@ -8,7 +14,7 @@ import edu.rpi.metpetdb.client.ui.MpDb;
 import edu.rpi.metpetdb.client.ui.ServerOp;
 
 public class SampleDetailsTest extends GWTTestCase {
-	
+
 	private User owner;
 
 	public String getModuleName() {
@@ -23,13 +29,14 @@ public class SampleDetailsTest extends GWTTestCase {
 		owner.setId(1);
 	}
 
-	public void testCreateSample() {
+	public void testSaveSample() {
 		final Sample s = new Sample();
-		s.setSesarNumber("123456789");
+		s.setSesarNumber("000000006");
 		s.setLatitude(1);
 		s.setLongitude(1);
 		s.setRockType("rockie rock");
 		s.setOwner(owner);
+		s.setAlias("6");
 		new ServerOp() {
 			public void begin() {
 				MpDb.sample_svc.saveSample(s, this);
@@ -37,11 +44,12 @@ public class SampleDetailsTest extends GWTTestCase {
 
 			public void onSuccess(final Object result) {
 				assertEquals(result, s);
+				System.out.println(result.toString());
 				finishTest();
 			}
-
 			public void onFailure(final Throwable e) {
 				e.printStackTrace();
+				System.out.println(e.getMessage());
 			}
 		}.begin();
 		delayTestFinish(5000);
@@ -53,44 +61,62 @@ public class SampleDetailsTest extends GWTTestCase {
 	public void testLoadSample() {
 		new ServerOp() {
 			public void begin() {
-				//MpDb.sample_svc.details(sampleId, this);
+				MpDb.sample_svc.details(1, this);
 			}
 
 			public void onSuccess(final Object result) {
-				//final Sample s = (Sample) result;
-				//assertTrue(s.getId() == sampleId);
+				final Sample s = (Sample) result;
+				assertTrue(s.getId() == 1);
+				System.out.println(s.toString());
 				finishTest();
 			}
+			
 		}.begin();
 		delayTestFinish(5000);
 	}
 
 	/**
-	 * Test if a sample can be deleted by first deleting the sample and then
-	 * attempting to load the sample again
+	 * Test if a sample can be deleted
 	 */
 	public void testDeleteSample() {
 		new ServerOp() {
 			public void begin() {
-				//MpDb.sample_svc.delete(sampleId, this);
+				MpDb.sample_svc.delete(1, this);
 			}
 
 			public void onSuccess(final Object result) {
-				new ServerOp() {
-					public void begin() {
-						///MpDb.sample_svc.details(sampleId, this);
-					}
-
-					public void onSuccess(final Object result) {
-						fail("Sample not deleted");
-					}
-					
-					public void onFailure(final Throwable e) {
-						assertTrue(true);
-						finishTest();
-					}
-				}.begin();
-				delayTestFinish(5000);
+				finishTest();
+			}
+		}.begin();
+		delayTestFinish(5000);
+	}
+	
+	public void testAllSamples() {
+		new ServerOp() {
+			public void begin() {
+				final PaginationParameters p = new PaginationParameters();
+				p.setAscending(true);
+				p.setMaxResults(10);
+				p.setOffset(0);
+				p.setParameter("alias");
+				MpDb.sample_svc.all(p, this);
+			}
+			public void onSuccess(final Object result) {
+				final Results results = (Results) result;
+				final List l = results.getList();
+				//verify the size
+				assertTrue(results.getSize() <= 10);
+				//Verify the order, also verifies we got the right ones
+				final Iterator itr = l.iterator();
+				//Start with 2 beause 1 was deleted
+				int count = 2;
+				while(itr.hasNext()) {
+					final Sample s = (Sample) itr.next();
+					System.out.println(s.toString());
+					assertEquals(s.getAlias(), String.valueOf(count));
+					++count;
+				}
+				finishTest();
 			}
 		}.begin();
 		delayTestFinish(5000);
