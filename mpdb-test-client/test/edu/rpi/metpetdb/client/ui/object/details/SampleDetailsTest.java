a24dc84425220e4f70d09d4a11cb49e0c99de148
anthony <anthony>
2008-02-05 00:27:41 +0000

    some more tests for client side samples

--- mpdb-test-client/test/edu/rpi/metpetdb/client/ui/object/details/SampleDetailsTest.java
+++ mpdb-test-client/test/edu/rpi/metpetdb/client/ui/object/details/SampleDetailsTest.java
@@ -13,6 +13,7 @@ import edu.rpi.metpetdb.client.model.User;
 import edu.rpi.metpetdb.client.ui.MpDb;
 import edu.rpi.metpetdb.client.ui.ServerOp;
 
+//TODO put in checks to make sure the exact amount comes back
 public class SampleDetailsTest extends GWTTestCase {
 
 	private User owner;
@@ -29,6 +30,10 @@ public class SampleDetailsTest extends GWTTestCase {
 		owner.setId(1);
 	}
 
+	/**
+	 * Test saving a sample to the database, which in turns test that
+	 * authentication works and the object constraints
+	 */
 	public void testSaveSample() {
 		final Sample s = new Sample();
 		s.setSesarNumber("000000006");
@@ -47,6 +52,7 @@ public class SampleDetailsTest extends GWTTestCase {
 				System.out.println(result.toString());
 				finishTest();
 			}
+
 			public void onFailure(final Throwable e) {
 				e.printStackTrace();
 				System.out.println(e.getMessage());
@@ -70,7 +76,7 @@ public class SampleDetailsTest extends GWTTestCase {
 				System.out.println(s.toString());
 				finishTest();
 			}
-			
+
 		}.begin();
 		delayTestFinish(5000);
 	}
@@ -90,7 +96,11 @@ public class SampleDetailsTest extends GWTTestCase {
 		}.begin();
 		delayTestFinish(5000);
 	}
-	
+
+	/**
+	 * Test loading all samples and verfies the correct amount, and the correct
+	 * order is returned
+	 */
 	public void testAllSamples() {
 		new ServerOp() {
 			public void begin() {
@@ -101,20 +111,83 @@ public class SampleDetailsTest extends GWTTestCase {
 				p.setParameter("alias");
 				MpDb.sample_svc.all(p, this);
 			}
+
 			public void onSuccess(final Object result) {
 				final Results results = (Results) result;
 				final List l = results.getList();
-				//verify the size
+				// verify the size
 				assertTrue(results.getSize() <= 10);
-				//Verify the order, also verifies we got the right ones
+				// Verify the order, also verifies we got the right ones
 				final Iterator itr = l.iterator();
-				//Start with 2 beause 1 was deleted
+				// Start with 2 beause 1 was deleted
 				int count = 2;
-				while(itr.hasNext()) {
+				while (itr.hasNext()) {
 					final Sample s = (Sample) itr.next();
 					System.out.println(s.toString());
 					assertEquals(s.getAlias(), String.valueOf(count));
 					++count;
+				}
+				finishTest();
+			}
+		}.begin();
+		delayTestFinish(5000);
+	}
+
+	/**
+	 * Basically the same as testAllSamples except it verifies each sample is
+	 * public
+	 */
+	public void testAllPublicSamples() {
+		new ServerOp() {
+			public void begin() {
+				final PaginationParameters p = new PaginationParameters();
+				p.setAscending(true);
+				p.setMaxResults(10);
+				p.setOffset(0);
+				p.setParameter("alias");
+				MpDb.sample_svc.allPublicSamples(p, this);
+			}
+
+			public void onSuccess(final Object result) {
+				final Results results = (Results) result;
+				final List l = results.getList();
+				// verify they are all public
+				final Iterator itr = l.iterator();
+				while (itr.hasNext()) {
+					final Sample s = (Sample) itr.next();
+					System.out.println(s.toString());
+					assertTrue(s.isPublicData());
+				}
+				finishTest();
+			}
+		}.begin();
+		delayTestFinish(5000);
+	}
+
+	/**
+	 * Basically the same as testAllSamples except it verifies that the user
+	 * owns the samples
+	 */
+	public void testAllSamplesForUser() {
+		new ServerOp() {
+			public void begin() {
+				final PaginationParameters p = new PaginationParameters();
+				p.setAscending(true);
+				p.setMaxResults(10);
+				p.setOffset(0);
+				p.setParameter("alias");
+				MpDb.sample_svc.allSamplesForUser(p, owner.getId(), this);
+			}
+
+			public void onSuccess(final Object result) {
+				final Results results = (Results) result;
+				final List l = results.getList();
+				// verify they are all public
+				final Iterator itr = l.iterator();
+				while (itr.hasNext()) {
+					final Sample s = (Sample) itr.next();
+					System.out.println(s.toString());
+					assertEquals(s.getOwner(), owner);
 				}
 				finishTest();
 			}
