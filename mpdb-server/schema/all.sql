2c88135901127422daae747876bd06eab2dddc46
gramm <gramm>
2008-03-25 23:32:56 +0000

    fixed a bug where elements_and_oxides.sql was being added before the tables were created (haha)

--- mpdb-server/schema/all.sql
+++ mpdb-server/schema/all.sql
@@ -1,7 +1,7 @@
-\i elements_and_oxides.sql
 \i metpetdb-core.sql
 \i metpetdb-image.sql
 \i metpetdb-chemical-analysis.sql
 \i metpetdb-perms.sql
 \i minerals.sql
+\i elements_and_oxides.sql
 \i sample-data.sql
