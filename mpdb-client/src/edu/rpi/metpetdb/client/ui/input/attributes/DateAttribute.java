d3f4a4e674270efd240550a74e8e342f9c1d209d
anthony <anthony>
2008-04-10 23:56:27 +0000

    made the date take into consideration of the precision when displaying fixed it so values entered in the textboxes are used as the date

--- mpdb-client/src/edu/rpi/metpetdb/client/ui/input/attributes/DateAttribute.java
+++ mpdb-client/src/edu/rpi/metpetdb/client/ui/input/attributes/DateAttribute.java
@@ -12,6 +12,7 @@ import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.widgetideas.client.events.ChangeEvent;
 import com.google.gwt.widgetideas.client.events.ChangeHandler;
+import com.google.gwt.widgetideas.datepicker.client.CalendarModel;
 import com.google.gwt.widgetideas.datepicker.client.DatePicker;
 
 import edu.rpi.metpetdb.client.model.MObjectDTO;
@@ -25,10 +26,12 @@ public class DateAttribute extends GenericAttribute {
 			"January", "February", "March", "April", "May", "June", "July",
 			"August", "September", "October", "November", "December",
 	};
-	private Timestamp newDate;
+	private Timestamp newDate = new Timestamp(0);
 	private final TextBox month = new TextBox();
 	private final TextBox day = new TextBox();
 	private final TextBox year = new TextBox();
+	private int daysInMonth = 30;
+	private int daysInYear = 365;
 
 	public DateAttribute(final TimestampConstraint sc, final ShortConstraint ic) {
 		super(new PropertyConstraint[] {
@@ -37,12 +40,20 @@ public class DateAttribute extends GenericAttribute {
 	}
 	public Widget[] createDisplayWidget(final MObjectDTO obj) {
 		return new Widget[] {
-			new MText(dateToString(get(obj)))
+			new MText(dateToString(get(obj), getPrecision(obj)))
 		};
 	}
 
 	public Widget[] createEditWidget(final MObjectDTO obj, final String id) {
 		final HorizontalPanel hp = new HorizontalPanel();
+
+		final Timestamp currentDate = get(obj);
+
+		if (currentDate != null) {
+			month.setText(currentDate.getMonth() + 1 + "");
+			day.setText(currentDate.getDate() + "");
+			year.setText(currentDate.getYear() + 1900 + "");
+		}
 
 		month.setMaxLength(2);
 		day.setMaxLength(2);
@@ -54,6 +65,7 @@ public class DateAttribute extends GenericAttribute {
 				new ClickListener() {
 					public void onClick(final Widget sender) {
 						final PopupPanel p = new PopupPanel(true);
+						final CalendarModel cm = new CalendarModel();
 						final DatePicker to = new DatePicker();
 						to.addChangeHandler(new ChangeHandler<Date>() {
 							public void onChange(ChangeEvent<Date> change) {
@@ -66,6 +78,10 @@ public class DateAttribute extends GenericAttribute {
 										+ 1900 + "");
 								newDate = new Timestamp(change.getNewValue()
 										.getTime());
+								cm.setCurrentMonthAndYear(newDate);
+								daysInMonth = cm
+										.getCurrentNumberOfDaysInMonth();
+
 							}
 						});
 						p.setWidget(to);
@@ -84,24 +100,32 @@ public class DateAttribute extends GenericAttribute {
 				chooseDate, hp
 		};
 	}
-
 	protected Object get(final Widget editWidget,
 			final PropertyConstraint constraint) {
 		if (constraint instanceof TimestampConstraint) {
+			if (month.getText().length() != 0)
+				newDate.setMonth(Integer.parseInt(month.getText()) - 1);
+			if (day.getText().length() != 0)
+				newDate.setDate(Integer.parseInt(day.getText()));
+			if (year.getText().length() != 0)
+				newDate.setYear(Integer.parseInt(year.getText()) - 1900);
 			return newDate;
 		} else if (constraint instanceof ShortConstraint) {
 			short datePrecision = 0;
 			if (month.getText().length() == 0)
 				datePrecision += 365;
 			else if (day.getText().length() == 0)
-				datePrecision += 30;
+				datePrecision += daysInMonth;
 			return datePrecision;
 		}
 		return null;
 	}
-
 	protected Timestamp get(final MObjectDTO obj) {
 		return (Timestamp) mGet(obj);
+	}
+
+	protected Short getPrecision(final MObjectDTO obj) {
+		return (Short) obj.mGet(this.getConstraints()[1].property);
 	}
 
 	protected void set(final MObjectDTO obj, final Object v,
@@ -113,7 +137,7 @@ public class DateAttribute extends GenericAttribute {
 
 	}
 
-	public final static String dateToString(final Date dt) {
+	public final static String dateToString(final Date dt, final Short precision) {
 		if (dt == null)
 			return "";
 
@@ -124,6 +148,11 @@ public class DateAttribute extends GenericAttribute {
 		final String m = months[month];
 		final String d = day < 10 ? "0" + day : String.valueOf(day);
 
-		return m + " " + d + ", " + String.valueOf(year);
+		if (precision == 0)
+			return m + " " + d + ", " + String.valueOf(year);
+		if (precision >= 365)
+			return year + "";
+		else
+			return m + " " + year;
 	}
 }
