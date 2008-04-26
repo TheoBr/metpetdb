b4ca326fc66c3137f78fdcc60d6500ab8673107a
anthony <anthony>
2008-04-26 03:58:24 +0000

    fixed error about SearchSample not being in hibernate, by the way why did you delete it in the first place?

---
+++ mpdb-server/src/edu/rpi/metpetdb/server/model/SearchSample.java
@@ -0,0 +1,12 @@
+package edu.rpi.metpetdb.server.model;
+
+public class SearchSample extends MObject {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public boolean mIsNew() {
+		return false;
+	}
+
+}
