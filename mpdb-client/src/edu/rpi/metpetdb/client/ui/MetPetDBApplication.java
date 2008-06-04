4fadb718db6b007c91e47f6a31ffb361eaffe95f
goldfd <goldfd>
2008-06-04 15:29:31 +0000

    MetPetDBApplication: Removed "Regenerate Constraints" from the Navigation Menu
    Been asked to remove it for the time being as the website will be shown to others. Commented out the lines that create MenuItem for "Regenerate Constraints"

--- mpdb-client/src/edu/rpi/metpetdb/client/ui/MetPetDBApplication.java
+++ mpdb-client/src/edu/rpi/metpetdb/client/ui/MetPetDBApplication.java
@@ -340,15 +340,15 @@ public class MetPetDBApplication implements EntryPoint {
 		// hdrnav.addItem(LocaleHandler.lc_text.faqMenu(), faq);
 		// hdrnav.addItem(LocaleHandler.lc_text.wikiMenu(), wiki);
 		hdrnav.addItem("test bulk upload", TokenSpace.bulkUpload);
-		hdrnav.addItem("regenerate constraints", new Command() {
-			public void execute() {
-				new VoidServerOp() {
-					public void begin() {
-						MpDb.mpdbGeneric_svc.regenerateConstraints(this);
-					}
-				}.begin();
-			}
-		});
+		// hdrnav.addItem("regenerate constraints", new Command() {
+		// public void execute() {
+		// new VoidServerOp() {
+		// public void begin() {
+		// MpDb.mpdbGeneric_svc.regenerateConstraints(this);
+		// }
+		// }.begin();
+		// }
+		// });
 
 	}
 
