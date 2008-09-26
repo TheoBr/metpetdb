582a42ddbe7d9a88207f9f7420e1cd0f56bbe6a5
watera2 <watera2>
2008-09-26 18:26:30 +0000

    samana only has java1.5 so we cant use isEmpty on strings

--- mpdb-common/src/edu/rpi/metpetdb/client/model/validation/ValueInCollectionConstraint.java
+++ mpdb-common/src/edu/rpi/metpetdb/client/model/validation/ValueInCollectionConstraint.java
@@ -50,7 +50,7 @@ public class ValueInCollectionConstraint extends PropertyConstraint implements
 
 	public void validateValue(Object value) throws ValidationException {
 		super.validateValue(value);
-		if (value != null && !value.toString().isEmpty()) {
+		if (value != null && value.toString().length() > 0) {
 			if (values != null && valueInCollection(value, this.getValues()))
 				return;
 			else if (value != null)
