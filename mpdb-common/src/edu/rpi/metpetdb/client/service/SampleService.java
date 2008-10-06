768b58281fc06f1fc6e40d33c9182d5ef280f9b1
watera2 <watera2>
2008-10-06 00:25:56 +0000

    fixed bug #155 and made it so when deleting subsamples it also deletes the grids

--- mpdb-common/src/edu/rpi/metpetdb/client/service/SampleService.java
+++ mpdb-common/src/edu/rpi/metpetdb/client/service/SampleService.java
@@ -3,7 +3,6 @@ package edu.rpi.metpetdb.client.service;
 import java.util.Collection;
 import java.util.Set;
 
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.RemoteService;
 
 import edu.rpi.metpetdb.client.error.DAOException;
