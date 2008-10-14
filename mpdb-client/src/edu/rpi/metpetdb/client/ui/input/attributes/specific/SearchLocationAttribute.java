d0cbd62014fb693e078f62ea74bf14d8c8678385
lindez <lindez>
2008-10-14 05:30:58 +0000

    changed starting coords and zoom level of location map

--- mpdb-client/src/edu/rpi/metpetdb/client/ui/input/attributes/specific/SearchLocationAttribute.java
+++ mpdb-client/src/edu/rpi/metpetdb/client/ui/input/attributes/specific/SearchLocationAttribute.java
@@ -66,7 +66,7 @@ public class SearchLocationAttribute extends SearchGenericAttribute implements
 		markerPoint1 = null;
 		markerPoint2 = null;
 
-		map = new MapWidget(LatLng.newInstance(0, 0),1);
+		map = new MapWidget(LatLng.newInstance(38.2, -39.7),2);
 		map.addControl(new LargeMapControl());
 		map.addControl(new MapTypeControl());
 		map.addControl(new ScaleControl());
