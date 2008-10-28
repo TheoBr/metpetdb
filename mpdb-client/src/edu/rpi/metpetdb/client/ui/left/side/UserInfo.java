aacbeb08785f74c6cefaab9e91ac00b1c683b1ec
lindez <lindez>
2008-10-28 15:03:38 +0000

    temporarily disabled user registration by: - removing Register link from logbar - removing register link from sidebar - replacing the form on the #register page with a 'temporarily disabled' message

--- mpdb-client/src/edu/rpi/metpetdb/client/ui/left/side/UserInfo.java
+++ mpdb-client/src/edu/rpi/metpetdb/client/ui/left/side/UserInfo.java
@@ -3,6 +3,7 @@ package edu.rpi.metpetdb.client.ui.left.side;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -13,9 +14,10 @@ import edu.rpi.metpetdb.client.ui.widgets.MLink;
 
 public class UserInfo extends LeftColWidget implements UsesLeftColumn {
 	private FlexTable ft;
+	private static final String LOGIN_ID = "login-link";
 
 	public UserInfo(final User user) {
-		super("User Information");
+		super("");
 		this.setStyleName("lcol-MyProjects");
 		// MetPetDBApplication.registerPageWatcher(this);
 		ft = new FlexTable();
@@ -27,33 +29,21 @@ public class UserInfo extends LeftColWidget implements UsesLeftColumn {
 				}
 			});
 			final MLink register = new MLink("register", TokenSpace.register);
-			HTML notLoggedIn = new HTML(
-					"<p class='notice'>You are not logged in.</p>");
-			FlexTable pleaseLogIn = new FlexTable();
-			pleaseLogIn.setWidget(0, 0, new Label("Please"));
-			pleaseLogIn.setWidget(0, 1, login);
-			pleaseLogIn.setWidget(0, 2, new Label("or"));
-			pleaseLogIn.setWidget(0, 3, register);
-			pleaseLogIn.setWidget(0, 4, new Label("to gain"));
-			Label pleaseLogIn2 = new Label("full access to MetPetDB's");
-			pleaseLogIn.setWidget(1, 0, pleaseLogIn2);
-			pleaseLogIn.setWidget(2, 0, new Label("features:"));
-			pleaseLogIn.getFlexCellFormatter().setColSpan(1, 0, 5);
-			pleaseLogIn.getFlexCellFormatter().setColSpan(2, 0, 5);
-			pleaseLogIn.getFlexCellFormatter().setWidth(0, 2, "16px");
-			pleaseLogIn.setWidth("196px");
-
-			HTML features = new HTML(
-					"<p><ul class='bullet'><li><strong>Upload sample data.</strong></li><li><strong>Save searches.</strong></li><li><strong>Create/Join projects.</strong></li></ul></p>");
-			ft.setWidget(0, 0, notLoggedIn);
-			ft.setWidget(1, 0, pleaseLogIn);
-			ft.setWidget(2, 0, features);
+			HTMLPanel notLoggedIn = new HTMLPanel(
+					"<p class=\"notice\">You are not logged in.</p>"+
+					"<p>Please <span id=\""+ LOGIN_ID + "\"></span> to gain full access to MetPetDB's features:</p>"+
+					"<ul class=\"bullet\">" +
+					"<li><strong>Upload sample data.</strong></li>" +
+					"<li><strong>Save searches.</strong></li>" +
+					"<li><strong>Create/Join projects.</strong></li>" +
+					"</ul>");
+			if (notLoggedIn.getElementById(LOGIN_ID) != null)
+				notLoggedIn.addAndReplaceElement(login, LOGIN_ID);
+			add(notLoggedIn);
 		} else {
 			Label username = new Label("Welcome, " + user.getName());
-			ft.setWidget(0, 0, username);
+			add(username);
 		}
-
-		this.add(ft);
 	}
 	public void onPageChanged() {
 		// MetPetDBApplication.removePageWatcher(this);
--- mpdb-client/src/edu/rpi/metpetdb/client/ui/user/UserRegistrationPanel.java
+++ mpdb-client/src/edu/rpi/metpetdb/client/ui/user/UserRegistrationPanel.java
@@ -85,11 +85,14 @@ public class UserRegistrationPanel extends MPagePanel implements ClickListener, 
 				});
 		p_main.edit(newbie);
 		
-		add(new OnEnterPanel(p_main) {
-			public void onEnter() {
-				doRegister();
-			}
-		});
+//		add(new OnEnterPanel(p_main) {
+//			public void onEnter() {
+//				doRegister();
+//			}
+//		});
+		
+		add(new MText("Automatic registration temporarily disabled. Please contact Frank Spear for an account."));
+		
 	}
 
 	protected void onLoad() {
