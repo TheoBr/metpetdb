9a71f24e09eeb66eb60774151c5d8015c4a531d9
goldfd <goldfd>
2009-04-28 03:36:17 +0000

    added a keyboard listener to the suggest box. hitting the up/down/enter buttons will now have the appropriate effects

--- mpdb-client/src/edu/rpi/metpetdb/client/ui/widgets/MSuggestText.java
+++ mpdb-client/src/edu/rpi/metpetdb/client/ui/widgets/MSuggestText.java
@@ -1,17 +1,19 @@
 package edu.rpi.metpetdb.client.ui.widgets;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.KeyboardListener;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MouseListener;
 import com.google.gwt.user.client.ui.PopupListener;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
@@ -20,7 +22,7 @@ import com.google.gwt.user.client.ui.ToggleButton;
 import com.google.gwt.user.client.ui.Widget;
 
 
-public class MSuggestText extends FlowPanel implements ClickListener {
+public class MSuggestText extends FlowPanel implements ClickListener, KeyboardListener, MouseListener {
 	public SuggestBox suggestBox;
 	private final String STYLENAME = "suggest";
 	private String popupStyleName;
@@ -31,6 +33,11 @@ public class MSuggestText extends FlowPanel implements ClickListener {
 	private final static String[] alphabet = {"0-9","@#$","A","B","C","D","E","F","G","H","I",
 		"J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
 	private final static String[] linkOptions = {"0-9","@#$","A-F","G-L","M-R","S-Z"};
+	private int selected;
+	private FlowPanel fp;
+	private ScrollPanel sp;
+	private FlowPanel linker;
+	private boolean isChemistry = false;
 	
 	public MSuggestText(){
 		this(new HashSet<String>(), false);
@@ -43,6 +50,7 @@ public class MSuggestText extends FlowPanel implements ClickListener {
 	
 	public MSuggestText(final Set<String> suggestions, final boolean addShowAll, final boolean isChemistry){
 		setStylePrimaryName(STYLENAME + "-box-wrap");
+		this.isChemistry = isChemistry;
 		MySuggestions = suggestions;
 		final MMultiWordSuggestOracle oracle = new MMultiWordSuggestOracle(isChemistry);	
 		oracle.addAll(suggestions);
@@ -91,11 +99,70 @@ public class MSuggestText extends FlowPanel implements ClickListener {
 		suggestBox.setText(s);
 	}
 	
+	public void onKeyUp(final Widget sender, final char c, final int i){
+		
+	}
+	
+	public void onKeyDown(final Widget sender, final char c, final int i){
+		if (Integer.valueOf(c) == KEY_DOWN){
+			selectedDown(sender);
+			setSelected();
+		} else if (Integer.valueOf(c) == KEY_UP){
+			selectedUp(sender);
+			setSelected();
+		} else if (Integer.valueOf(c) == KEY_ENTER){
+			selectionComplete((HTML)fp.getWidget(selected));
+		}
+	}
+	
+	public void onKeyPress(final Widget sender, final char c, final int i){
+		
+	}
+	
+	public void onMouseUp(final Widget sender, final int x, final int y){
+		
+	}
+	
+	public void onMouseDown(final Widget sender, final int x, final int y){
+		
+	}
+	
+	public void onMouseMove(final Widget sender, final int x, int y){
+		removeSelected(selected);
+		int low = 0;
+		int high = fp.getWidgetCount()-1;
+		int middle;
+		y+= sender.getAbsoluteTop();
+		while(high >= low){
+			middle = (low+high)/2;
+			if (y > fp.getWidget(middle).getAbsoluteTop() && y < fp.getWidget(middle).getAbsoluteTop() + fp.getWidget(middle).getOffsetHeight()){
+				if (fp.getWidget(middle) instanceof HTML){
+					selected = middle;
+				}
+				setSelected();
+				return;
+			} else if (y > fp.getWidget(middle).getAbsoluteTop() + fp.getWidget(middle).getOffsetHeight()){
+				low = middle+1;
+			} else {
+				high = middle-1;
+			}
+		}
+	}
+	
+	public void onMouseEnter(final Widget sender){	
+	}
+	
+	public void onMouseLeave(final Widget sender){
+	}
+	
+	
+	
 	public void onClick(final Widget sender){
+		final FocusPanel focusContainer = new FocusPanel();
 		final FlowPanel container = new FlowPanel();
-		final ScrollPanel sp = new ScrollPanel();
-		final FlowPanel fp = new FlowPanel();
-		final FlowPanel linker = new FlowPanel();
+		sp = new ScrollPanel();
+		fp = new FlowPanel();
+		linker = new FlowPanel();
 		linker.setStyleName("header");
 		final ArrayList<String> values = new ArrayList<String>(MySuggestions);
 		
@@ -134,24 +201,90 @@ public class MSuggestText extends FlowPanel implements ClickListener {
 				value.setStylePrimaryName("item");
 				value.addClickListener(new ClickListener(){
 					public void onClick(final Widget sender){
-						setText(getValue(value.getHTML()));
-						hide();
+						selectionComplete(value);
 					}
 				});
 				fp.add(value);
 			}
 		}
 		sp.add(fp);
-		
+		selected = getStringIndex(suggestBox.getText());
+		setSelected();
 		
 		container.add(linker);		
 		container.add(sp);
 		
-		db.setWidget(container);
+		
+		focusContainer.setWidget(container);
+		focusContainer.addKeyboardListener(this);
+		focusContainer.addMouseListener(this);
+		db.setWidget(focusContainer);
 
 		db.setPopupPosition(this.getAbsoluteLeft(), this.getAbsoluteTop()+this.getOffsetHeight());
 		db.show();
+		focusContainer.setFocus(true);
 	}
+	
+	private int getStringIndex(String input){
+		for (int i = 0; i < fp.getWidgetCount(); i++) {
+			final Widget w = fp.getWidget(i);
+			if (w instanceof HTML){
+				final String val = ((HTML) w).getText();
+				if (isChemistry){
+					final int spaceIndex = val.indexOf(160);	
+					if (spaceIndex > 0 && val.substring(0, spaceIndex).equalsIgnoreCase(input)){
+						return i;
+					}
+				} else {
+					if (val.equalsIgnoreCase(input)){
+						return i;
+					}
+				}
+			}
+		}
+		return 1;
+	}
+	
+	private void selectedDown(final Widget sender){
+		removeSelected(selected);
+		if (selected < fp.getWidgetCount()-1){
+			if (fp.getWidget(selected+1) instanceof HTML){
+				selected++;
+			} else selected+=2;
+			final Widget w = fp.getWidget(selected);
+			if (w.getAbsoluteTop()+w.getOffsetHeight() > sender.getAbsoluteTop() + sender.getOffsetHeight()){
+				sp.setScrollPosition(((w.getAbsoluteTop()-fp.getWidget(0).getAbsoluteTop())-sender.getOffsetHeight())+w.getOffsetHeight()+linker.getOffsetHeight());	
+			}
+		}
+	}
+	
+	private void selectedUp(final Widget sender){
+		removeSelected(selected);
+		if (selected > 1) {
+			if (fp.getWidget(selected-1) instanceof HTML){
+				selected--;
+			} else selected-=2;
+			final Widget w = fp.getWidget(selected);
+			if (w.getAbsoluteTop() < sender.getAbsoluteTop() + linker.getOffsetHeight()){
+				sp.setScrollPosition(w.getAbsoluteTop()-fp.getWidget(0).getAbsoluteTop());
+			}
+		}
+	}
+	
+	private void selectionComplete(final HTML selection){
+		setText(getValue(selection.getHTML()));
+		hide();
+	}
+	
+	private void removeSelected(final int index){
+		fp.getWidget(index).removeStyleDependentName("selected");
+	}
+	
+	private void setSelected(){
+		if (selected < fp.getWidgetCount() && selected >= 0){
+			fp.getWidget(selected).addStyleDependentName("selected");
+		}
+	} 
 	
 	private int findFirstMatch(final String criteria, final ArrayList<String> values){
 		for (int i = 0; i < values.size(); i++){
