5c85b734c673e809a01e4901666492b94e028c73
watera2 <watera2>
2009-05-07 20:03:54 +0000

    updated permissions for new tables

--- mpdb-server/schema/samana-perms.sql
+++ mpdb-server/schema/samana-perms.sql
@@ -46,8 +46,8 @@ alter table chemical_analysis_elements_archive  owner to metpetdb_dev;
 alter table chemical_analysis_oxides_archive  owner to metpetdb_dev;
 alter table subsamples_archive owner to metpetdb_dev;
 alter table roles owner to metpetdb_dev;
-alter table pending_roles owner to metpetdb_dev;
-alter table roles_changed owner to metpetdb_dev;
+alter table role_changes owner to metpetdb_dev;
+alter table mineral_relationships owner to metpetdb_dev;
 
 --triggers
 alter function archive_sample() owner to metpetdb_dev;
