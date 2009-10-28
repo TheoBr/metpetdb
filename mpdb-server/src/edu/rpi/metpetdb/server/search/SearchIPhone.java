9126a38774ab4e9fd20a1e77baddf2860a8c9bc1
buleth <buleth>
2009-10-28 23:25:23 +0000

    Creating XML output that will give a summary of the search criteria.

--- mpdb-server/src/edu/rpi/metpetdb/server/search/SearchIPhone.java
+++ mpdb-server/src/edu/rpi/metpetdb/server/search/SearchIPhone.java
@@ -56,6 +56,7 @@ public class SearchIPhone extends HttpServlet{
 	private static final String METAMORPHIC_GRADE="metamorphicGrade";
 	private static final String OWNER="owner";  
 	private static final String PAGINATION="pagination";
+	private static final String CRITERIA="criteriaSummary";
 
 	private Session session;
 	private Set<String> owners= new HashSet();
@@ -64,6 +65,7 @@ public class SearchIPhone extends HttpServlet{
 	private Set<Mineral> minerals= new HashSet();
 	private String region= new String();
 	private PaginationParameters p= new PaginationParameters();
+	private String criteria= "";
 	@Override
 
 	/*protected void doPost(final HttpServletRequest request,
@@ -223,7 +225,7 @@ public class SearchIPhone extends HttpServlet{
 			rockTypes= new HashSet();
 			metamorphicGrades= new HashSet();
 			minerals= new HashSet();
-			region= new String();
+			region= "";
 			p= new PaginationParameters();
 			//make sets of the various search criteria so they can be passed to the various search functions
 			if(request.getParameter(PAGINATION)!=null)
@@ -240,6 +242,7 @@ public class SearchIPhone extends HttpServlet{
 			{
 				p=null;	
 			}
+			
 			if(request.getParameter(OWNER)!= null)
 			{
 				String tempOwners[]  = request.getParameterValues(OWNER);
@@ -280,7 +283,10 @@ public class SearchIPhone extends HttpServlet{
 				}
 				//outputSearchXML(search(session), response);
 			}
-			
+			if(request.getParameter(CRITERIA)!= null)
+			{
+				criteria= request.getParameterValues(CRITERIA)[0];
+			}
 			// If there is a GET string for latitude and longitude then it is a search
 			if (request.getParameter(NORTH_PARAMETER) != null && request.getParameter(SOUTH_PARAMETER) != null && request.getParameter(WEST_PARAMETER)!= null && request.getParameter(EAST_PARAMETER) != null) {
 				double north = Double.parseDouble(request.getParameterValues (NORTH_PARAMETER)[0]);
@@ -289,21 +295,42 @@ public class SearchIPhone extends HttpServlet{
 				double east= Double.parseDouble(request.getParameterValues(EAST_PARAMETER)[0]);
 				
 				System.out.println("iPhone query: north = " + north + "south = " + south + "west = " + west + "east =" + east);
-				outputSearchXML(search(north,south, east, west, session),response);
-			} else if (request.getParameter(SEARCH_REGIONS) != null){
+				if(criteria.equals("true"))
+				{
+					getSearchCriteria(search(north,south,east,west, session), response);
+				}
+				else
+				{
+					outputSearchXML(search(north,south, east, west, session),response);
+				}
+			} 
+			else if (request.getParameter(SEARCH_REGIONS) != null){
 			//String region = new String();
 				for (String s : request.getParameterValues(SEARCH_REGIONS)){
 					if (s.length() > 2 && s.substring(0, 1).equals("'") && s.substring(s.length()-1, s.length()).equals("'")){
 						region=(s.substring(1, s.length()-1));
 					}
 				}
-				//outputSearchXML(search(region, session),response);
-				outputSearchXML(search(session), response);
+				if(criteria.equals("true"))
+				{
+					getSearchCriteria(search(session), response);
+				}
+				else
+				{
+					outputSearchXML(search(session), response);
+				}
 			}
 			//if search criteria were entered but a search region or search box was not, a seperate search must be done
 			else if(!minerals.isEmpty() || !owners.isEmpty() || !rockTypes.isEmpty() || !metamorphicGrades.isEmpty())
 			{
-				outputSearchXML(search(session), response);
+				if(criteria.equals("true"))
+				{
+					getSearchCriteria(search(session), response);
+				}
+				else
+				{
+					outputSearchXML(search(session), response);
+				}
 			}
 			else if (request.getParameter(SAMPLE_ID) != null){
 				for (String id : request.getParameterValues(SAMPLE_ID))
@@ -580,6 +607,173 @@ public class SearchIPhone extends HttpServlet{
 			throw new IllegalStateException(ioe.getMessage());
 		}
 	}
+	private void getSearchCriteria(final Results<Sample> results, final HttpServletResponse response) {
+		try{
+			final XStream x = new XStream();
+		response.getWriter().write("<searchCriteria>");
+		response.getWriter().write("<resultCount>");
+		x.toXML(results.getCount(), response.getWriter());
+		response.getWriter().write("</resultCount>");
+		
+		
+		//create a list of all minerals in all samples returned from search
+		Set <SampleMineral> currentMinerals = new HashSet();
+		List sampleList= results.getList();
+		Double maxLong= -180.0; //initialize to something smaller than it could possibly be so something is guaranteed to be larger
+		Double maxLat= -90.0;
+		Double minLong= 180.0;  //initialize to somthing bigger than it could be so something is guaranteed to be smaller
+		Double minLat= 90.0;
+		
+		for(int i=0; i<sampleList.size(); i++)
+		{
+			Sample samp= (Sample) sampleList.get(i);
+			if(((Point)samp.getLocation()).y > maxLat)
+			{
+				maxLat=((Point)samp.getLocation()).y;
+			}
+			if(((Point)samp.getLocation()).x > maxLong)
+			{
+				maxLong=((Point)samp.getLocation()).x;
+			}
+			if(((Point)samp.getLocation()).y < minLat)
+			{
+				minLat=((Point)samp.getLocation()).y;
+			}
+			if(((Point)samp.getLocation()).x < minLong)
+			{
+				minLong=((Point)samp.getLocation()).x;
+			}
+			Set<SampleMineral> mins= samp.getMinerals();
+			for(SampleMineral m : mins)
+			{
+				boolean flag=false;
+				for(SampleMineral currMin : currentMinerals)
+				{
+					if(currMin.getMineral().getId() == m.getMineral().getId())
+					{
+						flag=true;
+					}
+				}
+				if(flag==false)
+				{
+					currentMinerals.add(m);
+				}
+			}
+		}
+		//return xml for all the minerals
+		response.getWriter().write("<criteriaMinerals>");
+		for(SampleMineral min : currentMinerals)
+		{
+			x.toXML(min.getName(), response.getWriter());
+		}
+		response.getWriter().write("</criteriaMinerals>");
+		
+		//create a list of all metamorphic grades in all samples returned from search
+		
+		Set <MetamorphicGrade> currentMetGrades = new HashSet();
+		for(int i=0; i<sampleList.size(); i++)
+		{
+			Sample samp= (Sample) sampleList.get(i);
+			Set<MetamorphicGrade> metGrades= samp.getMetamorphicGrades();
+			for(MetamorphicGrade m : metGrades)
+			{
+				boolean flag=false;
+				for(MetamorphicGrade met : currentMetGrades)
+				{
+					if(met.getId() == m.getId())
+					{
+						flag=true;
+					}
+				}
+				if(flag==false)
+				{
+					currentMetGrades.add(m);
+				}
+			}
+		}
+		response.getWriter().write("<criteriaMetGrades>");
+		//return xml for all the metamorhpic grades
+		for(MetamorphicGrade met : currentMetGrades)
+		{
+			x.toXML(met.getName(), response.getWriter());
+		}
+		response.getWriter().write("</criteriaMetGrades>");
+		
+		
+		//create a list of all rock types of all samples returned from the search
+		Set <RockType> currentRockTypes = new HashSet();
+		for(int i=0; i<sampleList.size(); i++)
+		{
+			Sample samp= (Sample) sampleList.get(i);
+			RockType rock= samp.getRockType();
+			boolean flag=false;
+			for(RockType r : currentRockTypes)
+			{
+				if(r.getId()== rock.getId())
+				{
+					flag=true;
+				}
+			}
+			if(flag==false)
+			{
+				currentRockTypes.add(rock);
+			}
+		}
+		//return xml for all the rock types
+		response.getWriter().write("<criteriaRockTypes>");
+		for(RockType rt : currentRockTypes)
+		{
+			x.toXML(rt, response.getWriter());
+		}
+		response.getWriter().write("</criteriaRockTypes>");
+		
+		//create a list of all owners of all samples returned from the search
+		Set <String> currentOwners = new HashSet(); 
+		for(int i=0; i<sampleList.size(); i++)
+		{
+			Sample samp= (Sample) sampleList.get(i);
+			String owner= samp.getOwner().getName();
+			boolean flag=false;
+			for(String o : currentOwners)
+			{
+				if(o.equals(owner))
+				{
+					flag=true;
+				}
+			}
+			if(flag==false)
+			{
+				currentOwners.add(owner);
+			}
+		}
+		//return xml for all the rock types
+		response.getWriter().write("<criteriaOwners>");
+		for(String o : currentOwners)
+		{
+			x.toXML(o, response.getWriter());
+		}
+		response.getWriter().write("</criteriaOwners>");
+		response.getWriter().write("<maxLat>");
+		x.toXML(maxLat, response.getWriter());
+		response.getWriter().write("</maxLat>");
+		response.getWriter().write("<minLat>");
+		x.toXML(minLat, response.getWriter());
+		response.getWriter().write("</minLat>");
+		response.getWriter().write("<maxLong>");
+		x.toXML(maxLong, response.getWriter());
+		response.getWriter().write("</maxLong>");
+		response.getWriter().write("<minLong>");
+		x.toXML(minLong, response.getWriter());
+		response.getWriter().write("</minLong>");
+		
+		response.getWriter().write("</searchCriteria>");
+	}
+		
+		catch (final Exception ioe){
+			throw new IllegalStateException(ioe.getMessage());
+		}
+		
+	}
 	
 	private void outputSearchXML(final Results<Sample> results, final HttpServletResponse response){
 		try{
@@ -645,7 +839,6 @@ public class SearchIPhone extends HttpServlet{
 		try{
 			//if any search criteria have been specified (owners, rocktypes, metamorphic grades, or minerals)
 			//then set searchSample to have these attributes
-
 			SearchSample ss = new SearchSample();
 			if(region!=null && region.length()!=0)
 			{
@@ -715,8 +908,33 @@ public class SearchIPhone extends HttpServlet{
 			boundingBox.dimension = 2;
 			
 			s.setBoundingBox(boundingBox);
-			//return search(s, session);
-			return SearchDb.sampleSearch(null, s, null, session);
+			//return search(s, session);'
+		
+			if(!owners.isEmpty())
+			{
+				s.setOwners(owners);
+			}
+			if(!rockTypes.isEmpty())
+			{
+				s.setPossibleRockTypes(rockTypes);
+			}
+			if(!metamorphicGrades.isEmpty())
+			{
+				s.setMetamorphicGrades(metamorphicGrades);
+			}
+			if(!minerals.isEmpty())
+			{
+				s.setMinerals(minerals);
+			}
+			if(p==null)
+			{
+				return SearchDb.sampleSearch(null, s, null, session);
+			}
+			else
+			{
+				return SearchDb.sampleSearch(p, s, null, session);
+			}
+			//return SearchDb.sampleSearch(null, s, null, session);
 
 		} catch (final Exception ioe){
 			throw new IllegalStateException(ioe.getMessage());
