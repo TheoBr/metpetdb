782207ac7d5c91da9c95af7316cefde6168afcaa
Dennis <Dennis>
2010-02-09 01:47:16 +0000

    Added Jeff to the list of users created when rebuilding the database Uploaded the latest frameworks for postgres and postgis for snow leopard

--- mpdb-server/schema/users.sql
+++ mpdb-server/schema/users.sql
@@ -81,3 +81,4 @@ INSERT INTO users VALUES (nextval('user_seq'), 1, 'Zak Linder', 'lindez@rpi.edu'
 INSERT INTO users VALUES (nextval('user_seq'), 1, 'Nick Glickenhouse', 'glickn@rpi.edu','','','','','','','','','','Y',(select role_id from roles where rank=1));
 INSERT INTO users VALUES (nextval('user_seq'), 1, 'PUBLICATION', 'PUBLICATION', E'\\004c]\\333\\202\\210\\320D6\\337<\\315\\360@i\\327o\\227\\220\\205\\220lo\\030q','','','','','','','','','Y',(select role_id from roles where rank=1));
 INSERT INTO users VALUES (nextval('user_seq'), 1, 'Heather Buletti', 'buleth@rpi.edu', '','','','','','','','','','Y',(select role_id from roles where rank=1));
+INSERT INTO users VALUES (nextval('user_seq'), 1, 'Jeff Green', 'greenj12@rpi.edu', '','','','','','','','','','Y',(select role_id from roles where rank=1));
