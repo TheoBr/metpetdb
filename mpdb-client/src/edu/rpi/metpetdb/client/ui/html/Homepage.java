b4dffff120c7ed3a8e4f9ee11b6664f004bb266c
Jeff Green <Jeff Green>
2010-04-06 13:58:37 +0000

    - Edited home page to fit on one page. - Styled the table so it's not too bright and distracting. - Added border and padding to the table - Placed the image links to other databases next to iPhone App image link

--- mpdb-client/src/edu/rpi/metpetdb/client/ui/html/Homepage.java
+++ mpdb-client/src/edu/rpi/metpetdb/client/ui/html/Homepage.java
@@ -3,7 +3,7 @@ package edu.rpi.metpetdb.client.ui.html;
 import java.util.List;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
@@ -38,17 +38,19 @@ public class Homepage extends FlowPanel {
 		ImageHyperlink appLink = new ImageHyperlink(new com.google.gwt.user.client.ui.Image(GWT.getModuleBaseURL()+"/images/app-store.png"), 
 				"", TokenSpace.iphoneApp.getName(), false);		
 		appLink.getElement().setId("imageHyperlink");
+		appLink.addStyleName("l");
 		
 		bigContainer.getLeftCol().add(appLink);
 		bigContainer.getLeftCol().add(new HTML(
-						"<h3>Other \"Word\" Links</h3>" +
-						"<ul class=\"noBullets\">" +
-						"<li><span><a href=\"http://www.earthchem.org\" title=\"EarthChem\"><img src=\"images/earthchem-logo.jpg\" alt=\"EarthChem\"></a>Advanced Data Management in Solid Earth Geochemistry</span></li>" +
-						"<li><span><a href=\"http://www.petdb.org\" title=\"PETDB\"><img src=\"images/petdb-logo.jpg\" alt=\"PETDB\"></a>Petrological Database of the Ocean Floor</span></li>" +
-						"<li><span><a href=\"http://navdat.kgs.ku.edu\" title=\"NAVDAT\"><img src=\"images/navdat-logo.jpg\" alt=\"NAVDAT\"></a>The Western North American Volcanic and Intrusive Rock Database</span></li>" +
-						"<li><span><a href=\"http://georoc.mpch-mainz.gwdg.de/georoc/Start.asp\" title=\"GEOROC\"><img src=\"images/georoc-logo.jpg\" alt=\"GEOROC\"></a>Geochemistry of Rocks of the Ocean and Continents</span></li>" +
-						"<li><span><a href=\"http://www.geosamples.org\" title=\"SESAR\"><img src=\"images/sesar-logo.jpg\" alt=\"SESAR\"></a>System for Earth Sample Registration</span></li>" +
-						"</ul>"));
+						"<div class=\"smallColumn\">" +
+						"<a href=\"http://www.earthchem.org\" title=\"EarthChem\"><img class=\"linkIcon\" src=\"images/earthchem-logo.jpg\" alt=\"EarthChem\"></a></li>" +
+						"<a href=\"http://www.petdb.org\" title=\"PETDB\"><img class=\"linkIcon\" src=\"images/petdb-logo.jpg\" alt=\"PETDB\"></a>" +
+						"</div>" +
+						// "<a href=\"http://navdat.kgs.ku.edu\" title=\"NAVDAT\"><img class=\"linkIcon\" src=\"images/navdat-logo.jpg\" alt=\"NAVDAT\"></a>" +
+						"<div class=\"smallColumn\">" +
+						"<a href=\"http://georoc.mpch-mainz.gwdg.de/georoc/Start.asp\" title=\"GEOROC\"><img class=\"linkIcon\" src=\"images/georoc-logo.jpg\" alt=\"GEOROC\"></a>" +
+						"<a href=\"http://www.geosamples.org\" title=\"SESAR\"><img class=\"linkIcon\" src=\"images/sesar-logo.jpg\" alt=\"SESAR\"></a>" +
+						"</div>"));
 		
 		new ServerOp<List<List>>() {
 			@Override
@@ -56,18 +58,29 @@ public class Homepage extends FlowPanel {
 				MpDb.mpdbGeneric_svc.getStatistics(this);
 			}
 
-			public void onSuccess(final List<List> result) {				
-				bigContainer.getRightCol().add(new HTML("<img src=\"images/slices.jpg\">"));
+			public void onSuccess(final List<List> result) {
+				// Places the image on the right side of the page
+				bigContainer.getRightCol().add(new HTML("<img class=\"image-slices\" src=\"images/slices.jpg\">"));
 				
 				result.get(0);
-
-				for (int i = 0; i < result.get(0).size(); i++){
-					String listNumber = result.get(1).get(i).toString();
-					String listText = result.get(0).get(i).toString();
+				FlexTable holdTable = new FlexTable();
+				
+				// Loads the data into the table on the right
+				for (int i = 0; i < result.get(0).size(); i++) {
+					int row = holdTable.getRowCount();
+					Label listNumber = new Label(result.get(1).get(i).toString());
+					Label listText = new Label(result.get(0).get(i).toString());
+					holdTable.setWidget(i, 0, listNumber);
+					holdTable.setWidget(i, 1, listText);
 					
-					Label listRow = new Label(listNumber + listText);
-					listRow.setStyleName("listRow");
-					bigContainer.getRightCol().add(listRow);
+					// Styles each cell and the even and odd rows
+					holdTable.getCellFormatter().addStyleName(row, 0, "numberPart");
+					holdTable.getCellFormatter().addStyleName(row, 1, "wordPart");
+					if (i % 2 == 0) holdTable.getRowFormatter().addStyleName(row, "evenRow");
+					else holdTable.getRowFormatter().addStyleName(row, "oddRow");
+
+					// Adds each row table to the right column
+					bigContainer.getRightCol().add(holdTable);
 				}
 			}
 		}.begin();
