892d4bf2ee5dfd72cd601a8d95ea1f1fae26baf0
millib2 <millib2>
2010-06-25 20:33:17 +0000

    Replaced TreeAttribute with FlyOutAttribute, need to resolve conflicts with changes Patrick made, then will commit again

--- mpdb-client/src/edu/rpi/metpetdb/client/ui/input/attributes/specific/sample/MineralAttribute.java
+++ mpdb-client/src/edu/rpi/metpetdb/client/ui/input/attributes/specific/sample/MineralAttribute.java
@@ -3,22 +3,19 @@ package edu.rpi.metpetdb.client.ui.input.attributes.specific.sample;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.DisclosurePanel;
-import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.user.client.Window;
 
 import edu.rpi.metpetdb.client.model.Mineral;
 import edu.rpi.metpetdb.client.model.Sample;
@@ -28,33 +25,40 @@ import edu.rpi.metpetdb.client.model.validation.ObjectConstraint;
 import edu.rpi.metpetdb.client.model.validation.PropertyConstraint;
 import edu.rpi.metpetdb.client.model.validation.ValueInCollectionConstraint;
 import edu.rpi.metpetdb.client.ui.MpDb;
-import edu.rpi.metpetdb.client.ui.commands.ServerOp;
 import edu.rpi.metpetdb.client.ui.commands.VoidMCommand;
 import edu.rpi.metpetdb.client.ui.input.DetailsPanel;
-import edu.rpi.metpetdb.client.ui.input.DetailsPanelEntry;
 import edu.rpi.metpetdb.client.ui.input.MultipleObjectDetailsPanel;
 import edu.rpi.metpetdb.client.ui.input.WizardDialog;
+import edu.rpi.metpetdb.client.ui.input.attributes.FlyOutAttribute;
 import edu.rpi.metpetdb.client.ui.input.attributes.GenericAttribute;
 import edu.rpi.metpetdb.client.ui.input.attributes.TextAttribute;
-import edu.rpi.metpetdb.client.ui.input.attributes.TreeAttribute;
 import edu.rpi.metpetdb.client.ui.widgets.MHtmlList;
+import edu.rpi.metpetdb.client.ui.widgets.MHtmlList.ListItem;
 
-public class MineralAttribute extends GenericAttribute implements ClickListener {
+/**
+ * 
+ * @author millib2, ?
+ * 
+ * TODO: comments!!!
+ *
+ */
+public class MineralAttribute extends GenericAttribute<MObject> implements ClickHandler {
 
 	private final Button chooseMinerals;
 	private MObject obj;
-	private final SimplePanel container;
-	private TreeAttribute<Mineral> tree;
+	private final HorizontalPanel container;
+	
+	private FlyOutAttribute<Mineral> flyOutTree;
+	
 	private WizardDialog dialog;
-	private boolean choseMinerals = false;
+	private boolean mineralsChosen = false;
+	private DetailsPanel<MObject> p_mineral;
 
-	private DetailsPanel p_mineral;
-
-	final MultipleObjectDetailsPanel<SampleMineral> p_amounts = new MultipleObjectDetailsPanel<SampleMineral>(
+	private final MultipleObjectDetailsPanel<SampleMineral> p_amounts = 
+		new MultipleObjectDetailsPanel<SampleMineral>(
 			new GenericAttribute[] {
 				new TextAttribute(MpDb.doc.SampleMineral_Sample_minerals_amount)
-			}
-	);
+			});
 
 	public MineralAttribute(final ObjectConstraint<Mineral> mc) {
 		this(mc, 0);
@@ -63,106 +67,108 @@ public class MineralAttribute extends GenericAttribute implements ClickListener 
 	public MineralAttribute(final ValueInCollectionConstraint<Mineral> mc,
 			int maxMinerals) {
 		this((PropertyConstraint) mc, maxMinerals);
-	}
+	} 
 	
 	public MineralAttribute(final PropertyConstraint mc, int maxMinerals) {
 		super(mc);
 		chooseMinerals = new Button("Choose Minerals...", this);
-		container = new SimplePanel();
-		tree = new TreeAttribute<Mineral>(mc, 4, maxMinerals);
+		container = new HorizontalPanel();
+		flyOutTree = new FlyOutAttribute<Mineral>(mc, maxMinerals, false);
 	}
-	
-	
-	public Widget[] createDisplayWidget(final MObject obj) {	
-		FlexTable mineral_table = new FlexTable();
-		final Widget[] widgets;
-		MHtmlList tempList;
-		List<String> vals = new ArrayList<String>();
+
+	public Widget[] createDisplayWidget(final MObject obj) {
 		
-		widgets = tree.createDisplayWidget(obj);
-		tempList = (MHtmlList) ((SimplePanel) widgets[0]).getWidget();
-		Collection s = tempList.getItems();
+		final DisclosurePanel expand = new DisclosurePanel();
+		List<String> vals = new ArrayList<String>();		
+		this.obj = obj;
+		
+		addItemsToTree();
+		
+		final Widget[] widgets = flyOutTree.createDisplayWidget(obj);
+		// MHtmlList tempList = (MHtmlList) ((SimplePanel) widgets[0]).getWidget();
+		MHtmlList tempList = (MHtmlList) ((HorizontalPanel)widgets[0]).getWidget(0);
+		
+		Collection<ListItem> s = tempList.getItems();
 		if (s != null) {
-			final Iterator itr = s.iterator();
+			final Iterator<ListItem> itr = s.iterator();
 			while (itr.hasNext()) {
-				String rock = ((Label)((MHtmlList.ListItem) itr.next()).getWidget()).getText();
-				vals.add(rock);
+				vals.add(((Label)((MHtmlList.ListItem) itr.next()).getWidget()).getText());
 			}
-			Collections.sort(vals);
 		}
+		Collections.sort(vals);
 		
-		mineral_table.setStyleName("mineral_table");
-		mineral_table.setWidth("100%");
-		
-		int i;
-		int row = 0;
-		int col = 0;
-		for(i = 0; i < vals.size(); i++){
-			if(i % 10 == 0){
-				col++;
-				row = 0;
-			}
-			mineral_table.setText(row, col, vals.get(i));
-			row++;
+		MHtmlList list = new MHtmlList();
+		for (int i = 1; i < vals.size(); i++) {
+			list.add(new Label(vals.get(i)));
 		}
-		
+
+		if (vals.size() > 0) {
+			final Label closedText = new Label(vals.get(0));
+			expand.setHeader(closedText);
+		}
+		expand.add(list);
+		expand.setAnimationEnabled(true);
 		return new Widget[] {
-			mineral_table
+			expand
 		};
 	}
 
-	
 	private void remakeContainer(final MObject obj) {
-		this.obj = obj;
-		final Widget[] widgets;
-		MHtmlList tempList;
-		List<String> vals = new ArrayList<String>();
-		container.clear();
-		//TODO: Remove the copy & paste
-		widgets = tree.createDisplayWidget(obj);
-		tempList = (MHtmlList) ((SimplePanel) widgets[0]).getWidget();
-		Collection s = tempList.getItems();
-		if (s != null) {
-			final Iterator itr = s.iterator();
-			while (itr.hasNext()) {
-				String rock = ((Label)((MHtmlList.ListItem) itr.next()).getWidget()).getText();
-				vals.add(rock);
-			}
-			Collections.sort(vals);
-		}
 		
-		MHtmlList list = new MHtmlList();
-		for (int i = 0; i < vals.size(); i++) {
-			list.add(new Label(vals.get(i)));
+		this.obj = obj;
+		
+		addItemsToTree();
+		
+		container.clear();
+		
+		final Widget[] widgets = flyOutTree.createDisplayWidget(obj);
+		
+		for (int i = 0; i < widgets.length; ++i) {
+			container.add(widgets[i]);
+			System.out.println(widgets[i].toString());
 		}
-		container.add(list);
-		return;
 	}
 
-	
 	public Widget[] createEditWidget(final MObject obj, final String id,
-			final GenericAttribute ga) {
+			final GenericAttribute<MObject> ga) {
+		
 		final VerticalPanel vp = new VerticalPanel();
 		chooseMinerals.setEnabled(true);
 		this.obj = obj;
 		vp.add(chooseMinerals);
+		
 		remakeContainer(obj);
 		vp.add(container);
+		
 		return new Widget[] {
 			vp
 		};
 	}
-	
+
+	private void addItemsToTree() {
+		final ArrayList<Mineral> minerals = new ArrayList<Mineral>();
+		if (get(obj) != null) {
+			for (Object o : get(obj)) {
+				if (o instanceof SampleMineral)
+					minerals.add(((SampleMineral) o).getMineral());
+				else if (o instanceof Mineral)
+					minerals.add((Mineral) o);
+			}
+		}
+		
+		// tree.setSelectedItems(minerals);
+		flyOutTree.setSelectedItems(minerals);
+	}
 	
 	private WizardDialog makeWizardDialog() {
+		
 		final WizardDialog wd = new WizardDialog();
 
-		final GenericAttribute mineral_attributes[] = {
-			tree
+		final GenericAttribute<?> mineral_attributes[] = {
+			flyOutTree
 		};
 
-		p_mineral = new DetailsPanel(mineral_attributes, null, false) {
-			@Override
+		p_mineral = new DetailsPanel<MObject>(mineral_attributes, null, false) {
 			public boolean validateEdit(final VoidMCommand r) {
 				if (r != null)
 					r.execute();
@@ -175,30 +181,21 @@ public class MineralAttribute extends GenericAttribute implements ClickListener 
 		wd.turnOffValidation();
 
 		p_mineral.edit(this.obj);
-
+		
 		final Command mineral_amount = new Command() {
 			public void execute() {
-				// Before we edit the minerals we have to convert them to
-				// SampleMineral
-				final Iterator<Mineral> itr = tree.getSelectedItems()
-						.iterator();
-				final ArrayList<SampleMineral> newSelectedItems = new ArrayList<SampleMineral>();
-
-				while (itr.hasNext()) {
-					final Mineral mineral = itr.next();
-					final SampleMineral sampleMineral = new SampleMineral();
-					sampleMineral.setMineral(mineral);
-					newSelectedItems.add(sampleMineral);
-				}
-				p_amounts.edit(merge(tree.getSelectedItems(), ((Sample)obj).getMinerals()));
-				choseMinerals = true;
+				p_amounts.edit(merge(flyOutTree.getSelectedItems(), 
+						((Sample)obj).getMinerals()));
+				mineralsChosen = true;
 			}
 		};
+		
 		wd.addStep(p_mineral, 0, "Select Minerals");
 		if (this.getConstraint().equals(MpDb.doc.Sample_minerals)) {
 			wd.addTabChangeListener(mineral_amount);
 			wd.addStep(p_amounts, 1, "Enter Amounts");
 		}
+		
 		return wd;
 	}
 
@@ -221,79 +218,70 @@ public class MineralAttribute extends GenericAttribute implements ClickListener 
 		}
 		return merged;
 	}
-
-	protected Collection get(final GenericAttribute ga) {
-		return tree.get(ga);
+	
+	protected Collection<Mineral> get(final GenericAttribute<MObject> ga) {
+		return flyOutTree.get(ga);
 	}
 
 	protected void set(final MObject obj, final Object v) {
-		tree.set(obj, v);
+		flyOutTree.set(obj, v);
 	}
 
 	public Object get(final Widget editWidget) {
 		if (this.getConstraint().equals(MpDb.doc.Sample_minerals)) {
 			// return the sample minerals
-			if (choseMinerals)
+			if (mineralsChosen)
 				return p_amounts.getBeans();
 			else
 				return ((Sample)obj).getMinerals();
 		} else {
 			// return the minerals in the tree
-			return tree.get(editWidget);
+			return flyOutTree.get(editWidget);
 		}
 	}
 
-	protected Collection get(final MObject obj) {
+	protected Collection<?> get(final MObject obj) {
 		if (this.getConstraint().equals(MpDb.doc.Sample_minerals)) {
 			// return the sample minerals
 			return p_amounts.getBeans();
 		} else {
 			// return the minerals in the tree
-			return tree.get(obj);
+			return flyOutTree.get(obj);
 		}
 	}
-
+	
 	/**
-	 * Special version of contains that uses equals instead of hashcode
+	 * Called when the user clicks the "Choose minerals..." button.
 	 * 
-	 * @param c
-	 * @param o
-	 * @return
+	 * @param event
+	 * 		The GWT ClickEvent object associated with the user's click action. 
 	 */
-	private Object containsObject(final Collection c, final Object o) {
-		if (c == null || o == null)
-			return null;
-		final Iterator itr = c.iterator();
-		while (itr.hasNext()) {
-			final Object object = itr.next();
-			if (o.equals(object)) {
-				return object;
-			}
-		}
-		return null;
-	}
-
-	public void onClick(final Widget sender) {
-		if (sender == chooseMinerals) {
+	public void onClick(ClickEvent event) {
+		// Create and show the select minerals dialog
+		if (event.getSource() == chooseMinerals) {
+			
 			if (dialog == null || !(p_mineral.getBean().equals(this.obj)))
 				dialog = makeWizardDialog();
+			
+			// Command to be executed when the user is finished with the 
+			// select minerals dialog
 			final Command dialog_finish = new Command() {
 				public void execute() {
 					if (getConstraint().equals(MpDb.doc.Sample_minerals)){
 						p_amounts.validateEdit(null);
-						tree.set(obj, get(obj));
+						flyOutTree.set(obj, get(obj));
 					}
-					else
+					else {
 						//this is a hack to make it set the value the the user selected
-						tree.set(obj, get(new Label()));
-					MineralAttribute.this
-							.remakeContainer(MineralAttribute.this.obj);
+						flyOutTree.set(obj, get(new Label()));
+					}
+					MineralAttribute.this.remakeContainer(MineralAttribute.this.obj);
 				}
 			};
+			
 			dialog.clearDialogFinishListeners();
 			dialog.addDialogFinishListener(dialog_finish);
 			dialog.show();
 		}
 	}
-
 }
