a25d6948cc4e7ce25b936115867b37765ca3a40f
Stephen Ball <sball@LAB.CS.RPI.EDU>
2010-10-15 19:46:47 +0000

    database changes made

---
+++ mpdb-server/schema/migrations/10_16_10.sql
@@ -0,0 +1,5 @@
+alter table users add column contributor_code char(32);
+alter table users add column contributor_enabled char(1);
+alter table users add column professional_url varchar(255);
+alter table users add column research_interests varchar(1024);
+alter table users add column request_contributor char(1);
