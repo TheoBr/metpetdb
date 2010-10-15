cff9383b12684a5875699aa63f7bcbb11ab90eef
Stephen Ball <sball@LAB.CS.RPI.EDU>
2010-10-15 18:55:55 +0000

    UserRegistration, part 3

--- mpdb-server/src/edu/rpi/metpetdb/server/EmailSupport.java
+++ mpdb-server/src/edu/rpi/metpetdb/server/EmailSupport.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.Message.RecipientType;
@@ -23,9 +24,18 @@ public class EmailSupport {
 	private static InternetAddress fromAddress;
 	private static Properties allProperties;
 
-	private static synchronized void load() throws MessagingException {
+	
+	public static String getApproverAddress() throws MessagingException
+	{
+		if (allProperties == null)
+			EmailSupport.load();
+		
+		return allProperties.getProperty("approver.address");
+	}
+	
+	public static synchronized Properties load() throws MessagingException {
 		if (smtpServer != null)
-			return;
+			return null;
 		final String propFile = "email.properties";
 		final InputStream i = EmailSupport.class.getClassLoader()
 				.getResourceAsStream(propFile);
@@ -44,14 +54,37 @@ public class EmailSupport {
 				props.getProperty("from.name")));
 
 		allProperties = props;
+		
+		return allProperties;
 	}
 
+	
 	private static Session newSession() throws MessagingException {
 		load();
 		final Properties p = new Properties();
 		p.put("mail.smtp.host", smtpServer);
 		return Session.getInstance(p, null);
 	}
+	
+/*	private static Session newGmailSession() throws MessagingException {
+		load();
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");		
+    	props.put("mail.smtp.socketFactory.port", "465");
+    	props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+    	props.put("mail.smtp.port", "465");
+ 
+    	
+		Session session = Session.getDefaultInstance(props,
+				new javax.mail.Authenticator() 
+		{
+			protected PasswordAuthentication getPasswordAuthentication()
+			{ return new PasswordAuthentication("XXXXXXX","XXXXXXX");	}
+		});
+		
+    	return session;
+    	}*/
 
 	/**
 	 * Send a single message to one address.
@@ -88,6 +121,7 @@ public class EmailSupport {
 			throw new UnableToSendEmailException();
 		}
 	}
+
 	private EmailSupport() {
 	}
 }
